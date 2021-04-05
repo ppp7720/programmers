dfd567901d9dc3fa10065d3d276dd5f52f545042
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-04-05 14:54:10 +0900

    Add files via upload

--- 소수 찾기.py
+++ 소수 찾기.py
@@ -1,52 +1,39 @@
-n = 1000
+numbers	= "011"
 
-def solution1(n):
-    result = list(range(2, n+1))
+from itertools import permutations
 
-    for num in result:
-        for i in range(num*2, n+1, num):
-            if i in result:
-                result.remove(i)
-    return len(result)
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True if n >= 2 else False
 
-def solution2(n):
-    a = [False,False] + [True]*(n-1)
-    primes=[]
 
-    for i in range(2,n+1):
-        if a[i]:
-            primes.append(i)
-            for j in range(i*2, n+1, i):
-                a[j] = False
-    return len(primes)
+l = [int(''.join(i)) for j in range(1,len(numbers)+1) for i in permutations(numbers,j)]
 
-def solution3(n):
-    result = set(range(2,n+1))
+result = [prime(i) for i in set(l)]
 
-    for i in range(2,n+1):
-        if i in result:
-            result -= set(range(i*2,n+1,i))
-
-    return len(result)
-
-print(solution3(n))
+print(l)
 
 # 소수 찾기
 # 문제 설명
-# 1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+# 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
 
-# 소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
-# (1은 소수가 아닙니다.)
+# 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 
-# 제한 조건
-# n은 2이상 1000000이하의 자연수입니다.
+# 제한사항
+# numbers는 길이 1 이상 7 이하인 문자열입니다.
+# numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+# "013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
 # 입출력 예
-# n	result
-# 10	4
-# 5	3
+# numbers	return
+# "17"	3
+# "011"	2
 # 입출력 예 설명
-# 입출력 예 #1
-# 1부터 10 사이의 소수는 [2,3,5,7] 4개가 존재하므로 4를 반환
+# 예제 #1
+# [1, 7]으로는 소수 [7, 17, 71]를 만들 수 있습니다.
 
-# 입출력 예 #2
-# 1부터 5 사이의 소수는 [2,3,5] 3개가 존재하므로 3를 반환
+# 예제 #2
+# [0, 1, 1]으로는 소수 [11, 101]를 만들 수 있습니다.
+
+# 11과 011은 같은 숫자로 취급합니다.
